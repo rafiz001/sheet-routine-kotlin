65680dd70d1cd078150a0bf896123a93571a83fa
Md. Rafiz Uddin <rafiz001@protonmail.ch>
2026-01-04 22:47:20 +0600

    learned padding, app bar, layout

--- app/src/main/java/com/example/sheetroutine/MainActivity.kt
+++ app/src/main/java/com/example/sheetroutine/MainActivity.kt
@@ -1,28 +1,64 @@
 package com.example.sheetroutine
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sheetroutine.ui.theme.SheetRoutineTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SheetRoutineTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                Scaffold(
+                    topBar = {
+                        TopAppBar(title = {
+                            Text("Rafiz")
+                        },
+                            navigationIcon = {
+                                IconButton(onClick = {
+
+                                }) {
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                        contentDescription = "Back"
+                                    )
+                                }
+                            }
+                        )
+
+                    },
+                    ) { innerPadding ->
+
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        name = "Androiaaa",
+                        modifier = Modifier.padding(top = innerPadding.calculateTopPadding(), bottom = 0.dp)
                     )
                 }
             }
@@ -32,10 +68,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            //.background(color = Color.Cyan)
+
+    ) {
+        for (i in 0..100){
+            Text(
+                text = "Hello $name! $i",
+                color = Color.Cyan
+
+            )
+            HorizontalDivider()
+        }
+
+
+    }
+
 }
 
 @Preview(showBackground = true)
